415c05ba22e28bf4d1889c419744464ce038e539
LesnoyPudge <chuykin.a01@gmail.com>
2025-02-09 23:50:20 +0500

    'update'

--- src/root/Root/components/Router/routes/DevRoutes/components/Playground/sections/TMP/index.tsx
+++ src/root/Root/components/Router/routes/DevRoutes/components/Playground/sections/TMP/index.tsx
@@ -11,45 +11,65 @@ import { v4 as uuid } from 'uuid';
 
 export const TMP: FC = () => {
     // const db1 = useConst(() => new FakeDB());
-    const [state, setState] = useState(db.createEmptyStorage());
+    // const [state, setState] = useState(db.createEmptyStorage());
 
-    useTimeout(() => {
-        db.clearStorage();
+    // useTimeout(() => {
+    //     db.clearStorage();
 
-        const id = uuid();
-        const { accessToken, refreshToken } = token.generateTokens({
-            id,
-            password: 'zxc',
-        });
+    //     const id = uuid();
+    //     const { accessToken, refreshToken } = token.generateTokens({
+    //         id,
+    //         password: 'zxc',
+    //     });
 
-        const user = db.create('user', Dummies.user({
-            id,
-            accessToken,
-            refreshToken,
-            login: 'qwe',
-            password: 'zxc',
-            name: 'qwezxc',
-        }));
+    //     const user = db.create('user', Dummies.user({
+    //         id,
+    //         accessToken,
+    //         refreshToken,
+    //         login: 'qwe',
+    //         password: 'zxc',
+    //         name: 'qwezxc',
+    //     }));
 
-        scenarios.populate(user.id);
+    //     scenarios.populate(user.id);
 
-        // db1.create('user', Dummies.user({
-        //     id: 'qwe',
-        //     accessToken: 'zxczxc',
-        //     login: 'qwe',
-        //     name: 'qwezxc',
-        //     password: 'zxc',
-        //     refreshToken: 'qweqwe',
-        // }));
+    //     // db1.create('user', Dummies.user({
+    //     //     id: 'qwe',
+    //     //     accessToken: 'zxczxc',
+    //     //     login: 'qwe',
+    //     //     name: 'qwezxc',
+    //     //     password: 'zxc',
+    //     //     refreshToken: 'qweqwe',
+    //     // }));
 
-        setState(db.getStorageClone());
-    }, 3_000);
+    //     setState(db.getStorageClone());
+    // }, 3_000);
+
+    useEffect(() => {
+        console.log('!!!');
+        localStorage.clear();
+        const orig = Object.getPrototypeOf(localStorage).setItem.bind(
+            localStorage,
+        );
+        Object.getPrototypeOf(localStorage).setItem = (...args: any[]) => {
+            console.log('hehe');
+            // orig.call(localStorage, ...args);
+            orig(...args);
+        };
+
+        localStorage.setItem('?', '1');
+        // orig.call(localStorage, '?', '1');
+
+        console.log(localStorage.getItem('?'));
+
+        console.log(localStorage);
+    }, []);
 
     return (
         <div className='h-screen w-screen'>
-            <Scrollable className='flex flex-col gap-4 p-4'>
+            {/* <Scrollable className='flex flex-col gap-4 p-4'>
                 <JsonView data={JSON.stringify(state) ?? ''}/>
-            </Scrollable>
+            </Scrollable> */}
         </div>
     );
 };
